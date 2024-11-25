8881599437aba2d718c0085550fe540ea7ca1bb1
giordana <foglia.2046738@studenti.uniroma1.it>
2024-11-25 13:41:59 +0100

    ciao

--- service/api/login-user.go
+++ service/api/login-user.go
@@ -10,32 +10,30 @@ import (
 
 // Gestione del login e creazione dell'utente
 func (rt *_router) loginUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
-	// Decoding the JSON containing the username
 	var req LoginRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
 
-	// Validating the username
+	// controllo username
 	if req.User == "" {
 		http.Error(w, "Missing 'Username' parameter", http.StatusBadRequest)
 		return
 	}
 
-	// Getting the UId from the database
+	// prendo l'id dell'utente dal database
 	userID, err := rt.db.GetIdUser(req.User)
 	if err != nil {
 		http.Error(w, "Server error", http.StatusInternalServerError)
 		return
 	}
 
-	// Creating the response
+	// assegno userId
 	response := LoginResponse{
 		User_ID: userID,
 	}
 
-	//ctx.Logger.Infof()
 	ctx.Logger.Infof("login successful")
 
 	w.Header().Set("Content-Type", "application/json")
