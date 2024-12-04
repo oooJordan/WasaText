491fabba11b51922a7ee22c33714900839128dea
giordana <foglia.2046738@studenti.uniroma1.it>
2024-12-04 09:34:20 +0100

    tolto fmt

--- service/database/get-id-user.go
+++ service/database/get-id-user.go
@@ -3,14 +3,12 @@ package database
 import (
 	"database/sql"
 	"errors"
-	"fmt"
 )
 
 func (db *appdbimpl) GetIdUser(name string) (int, error) {
 	var user_id UserIdDatabase
 	err := db.c.QueryRow("SELECT user_id FROM users WHERE name = ?", name).Scan(&user_id.User_ID)
 	if err != nil {
-		fmt.Println(err.Error())
 		if errors.Is(err, sql.ErrNoRows) {
 			DefaultImage := "https://example.com/default-profile-image.png"
 			result, err := db.c.Exec("INSERT INTO users (name, profile_image) VALUES (?, ?)", name, DefaultImage)
